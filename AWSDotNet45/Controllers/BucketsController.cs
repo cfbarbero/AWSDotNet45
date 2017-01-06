daf0e96d492e5bad4d66234037652576a3638787
crisb <cfbarbero@gmail.com>
2017-01-06 13:16:01 -0700

    Initial Commit

---
+++ AWSDotNet45/Controllers/BucketsController.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AWSDotNet45.Controllers
+{
+    public class BucketsController : ApiController
+    {
+        private IAmazonS3 S3Client;
+
+        public BucketsController()
+        {
+            this.S3Client = new Amazon.S3.AmazonS3Client();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var buckets = S3Client.ListBucketsAsync();
+
+            return buckets.Result.Buckets.Select(x => x.BucketName);
+        }
+    }
+}
---
+++ AWSDotNet45/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AWSDotNet45.WebApiApplication" Language="C#" %>
